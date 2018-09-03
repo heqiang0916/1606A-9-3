3a06c0296bab853ffb828ecdece47fda79e5a282
heqiang <1716408099@qq.com>
2018-09-03 09:27:22 +0800

    整合任务

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,19 @@ var minjs = require('gulp-uglify');
 var server = require('gulp-webserver');
 //压缩scss
 gulp.task('duiscss', function() {
-    return gulp.src('./src/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./src/css'))
-})
+        return gulp.src('./src/scss/*.scss')
+            .pipe(mincss())
+            .pipe(gulp.dest('./src/css'))
+    })
+    //监听scss
+gulp.task('watch', function() {
+        return gulp.watch('./src/scss/*.scss', gulp.series('duiscss'));
+    })
+    //压缩js
+gulp.task('duijs', function() {
+        return gulp.src("./src/js/*.js")
+            .pipe(minjs())
+            .pipe(gulp.dest('./src/publit'))
+    })
+    //整合任务
+gulp.task('dev', gulp.series('duiscss', 'duijs', 'watch'))
